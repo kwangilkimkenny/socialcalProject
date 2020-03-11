fe81df52cb783509d698af53a157e1bc5f59b946
kyle <cackikim@naver.com>
2020-03-11 15:30:50 +0900

    added view and temples etc...

---
+++ blog/templates/socialActFrogiveness.py
@@ -0,0 +1,17 @@
+print("모든 것의 가치를 산정한다는 것은 어려운 일이다. \n하지만 모든 것은 가치가 있다. 공짜라고 진짜 가치가 없는 것은 아니다. \n공기가 없다면 생명체는 살 수 없다. \n맑은 공기를 마시시 위해서 우리가 지불해야하는 가치(비용)은 얼마가 될까. \n 이 계산기는 이러한 문제를 계산해보고 가진것들의 가치를 다시한번 생각해 보자.")
+print()
+x=  input("activity 'The value of forgiveness. How many times will you forgive others?': ")
+
+inputA= float(x) 
+
+VOF  = inputA * (7500/2)
+
+
+print()
+print("Forgiveness is like the effect of meditation physically because it brings peace of mind. Meditation can prevent cardiovascular disease. Prevent arrhythmia or heart attack.")
+print("The cost of treatment for arrhythmia is $5,000.")
+print("The cost of cardiovascular treatment is $2,500.")
+print("...")
+print()
+print("The value of the number of times you forgive:$", round(VOF))
+
